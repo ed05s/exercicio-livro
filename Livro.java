17b2877862447d544c9bc856bd958255d491fa07
ed05s <edinaldolouredomarques@gmail.com>
2021-12-15 15:38:53 -0300

    first commit

---
+++ Livro.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetolivro;
+
+/**
+ *
+ * @author ADMIN
+ */
+// implementando a interface Publicação na classe Livro
+public class Livro implements Publicacao {
+    private String titulo;
+    private String autor;
+    private int totPaginas;
+    private int pagAtual;
+    private boolean aberto;
+    private Pessoa leitor;
+
+    
+           
+
+    public String detalhes() {
+        return "Livro{" + "titulo = "
+                + titulo + "\n, autor = " + autor + ", totPaginas = " + totPaginas +
+                ", pagAtual = " + pagAtual + ", aberto = " + aberto + "\n, leitor = "
+                + leitor.getNome() + ", idade = "
+                + leitor.getIdade() + ", sexo = "
+                + leitor.getSexo() + '}';
+    }
+
+    public Livro(String titulo, String autor, int totPaginas, Pessoa leitor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.totPaginas = totPaginas;
+        this.leitor = leitor;
+    }
+     
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getTotPaginas() {
+        return totPaginas;
+    }
+
+    public void setTotPaginas(int totPaginas) {
+        this.totPaginas = totPaginas;
+    }
+
+    public int getPagAtual() {
+        return pagAtual;
+    }
+
+    public void setPagAtual(int pagAtual) {
+        this.pagAtual = pagAtual;
+    }
+
+    public boolean isAberto() {
+        return aberto;
+    }
+
+    public void setAberto(boolean aberto) {
+        this.aberto = aberto;
+    }
+
+    public Pessoa getLeitor() {
+        return leitor;
+    }
+
+    public void setLeitor(Pessoa leitor) {
+        this.leitor = leitor;
+    }
+
+    @Override
+    public void abrir() {
+        this.aberto=true;
+    }
+       
+
+    @Override
+    public void fechar() {
+        this.aberto=false;
+    }
+
+    @Override
+    public void folhear(int p) {
+        
+        if(p>this.totPaginas){
+            System.out.println("não é possivel folear até esta pagina pois este livro tem apenas "
+                    + this.totPaginas); 
+        }else{
+            this.pagAtual=p;
+        }
+    }
+
+    @Override
+    public void avancarPag() {
+      this.pagAtual++;
+    }
+
+    @Override
+    public void voltarPag() {
+       this.pagAtual--; 
+    }
+     
+    
+    
+}
---
+++ Pessoa.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetolivro;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String sexo;
+
+    public Pessoa(String nome, int idade, String sexo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+      public void aniversário(){
+        this.idade=this.idade++;
+    }
+    
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    
+  
+  }
+   
